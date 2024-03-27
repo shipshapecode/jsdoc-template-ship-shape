061a5c600b28a4203bea206c691ddb55c3120482
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2024-03-27 20:22:31 +0800

    fix anchor error (#1)

--- static/scripts/linenumber.js
+++ static/scripts/linenumber.js
@@ -2,23 +2,24 @@
 
 /* global document */
 (function () {
-  var lineId, lines, totalLines, anchorHash;
+  var lineId, lines, anchorHash;
   var source = document.getElementsByClassName('prettyprint source linenums');
-  var i = 0;
   var lineNumber = 0;
 
   if (source && source[0]) {
     anchorHash = document.location.hash.substring(1);
-    lines = source[0].getElementsByTagName('li');
-    totalLines = lines.length;
-
-    for (; i < totalLines; i++) {
-      lineNumber++;
-      lineId = 'line' + lineNumber;
-      lines[i].id = lineId;
+    lines = source[0].innerHTML.split('\n');
+    for (; lineNumber < lines.length; lineNumber++) {
+      lineId = 'line' + (lineNumber + 1);
+      lines[lineNumber] =
+        '<span id="' + lineId + '"></span>' + lines[lineNumber];
       if (lineId === anchorHash) {
-        lines[i].className += ' selected';
+        lines[lineNumber] = lines[lineNumber].replace(
+          '<span id="line',
+          '<span class="selected" id="line'
+        );
       }
     }
+    source[0].innerHTML = lines.join('\n');
   }
 })();
